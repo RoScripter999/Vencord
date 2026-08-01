0d1d044334845fe1c7148c57fd86249d83c967d1
RoScripter999 <orel.afriat2000@gmail.com>
2026-08-01 01:24:54 +0100

    Revert "fix edge case where Discord installs can go corrupt (#4472)"
    This reverts commit 738b7e44a6e91b8b3a58f47f6c39f3373bd5cd12.

--- src/main/persistAfterDiscordUpdates.ts
+++ src/main/persistAfterDiscordUpdates.ts
@@ -54,7 +54,7 @@ function patchLatest() {
         const newAppAsar = join(resources, "app.asar");
         const newAppAsarBackup = join(resources, "_app.asar");
 
-        if (!existsSync(oldVencordAsar) || !existsSync(newAppAsar) || existsSync(newAppAsarBackup)) return;
+        if (!existsSync(oldVencordAsar) || !existsSync(newAppAsar)) return;
 
         console.info(`[Vencord] Detected Host Update (${currentVersion} -> ${latestVersion}). Repatching...`);
 
